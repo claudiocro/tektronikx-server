314792b49a1ac5a282d27858ac91f0528ce8b4ab
Claudio Romano <claudiocro@gmail.com>
2015-08-18 14:38:49 +0200

    fix(policy): fix isAdmin policy

--- api/policies/isAdmin.js
+++ api/policies/isAdmin.js
@@ -1,8 +1,7 @@
 module.exports = function(req, res, next) {
-  if(!req.user.userType !== 3){
-    res.status(500).send('Something broke!');
-    return res.status(401).send({
-      message: 'Request is not authorized'
+  if(req.user.userType !== 3){
+    return res.status(500).send({
+      message: 'Request is not authorized' + req.user.userType
     });
   } else {
     next();
